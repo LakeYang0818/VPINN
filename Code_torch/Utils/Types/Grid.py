Correct 2d test functions and grid volume computation

Code_torch/Utils/Types/Grid.py
@@ -57,9 +57,9 @@ class Grid:
             requires_grad = False
 
         if y is not None:
-            if type(x) != type(y):
-                raise ValueError(f"Arguments 'x' and 'y' must be of same type, but are of types "
-                                 f"{type(x)} and {type(y)}")
+            # if type(x) != type(y):
+            #     raise ValueError(f"Arguments 'x' and 'y' must be of same type, but are of types "
+            #                      f"{type(x)} and {type(y)}")
             if isinstance(y, torch.Tensor):
                 if y.size() == torch.Size([0]):
                     raise ValueError(f"Invalid arg 'y' for grid: cannot generate grid from empty tensor.")
@@ -70,22 +70,21 @@ class Grid:
                     warnings.warn(f"You have passed a {type(y)} argument for 'y' but set 'as_tensor = False'. "
                                   f"In this case, 'as_tensor' will be set to true.")
                     as_tensor = True
-                else:
-                    self._n_y = y.size()[0]
+                self._n_y = y.size()[0]
             else:
                 if len(y) == 0:
                     raise ValueError(f"Invalid arg 'y' for grid: cannot generate grid from empty {type(y)}.")
                 else:
-                    self._n_y = len(x)
+                    self._n_y = len(y)
             self._dim = 2
         else:
             self._dim = 1
 
         # Create coordinate axes
         if not as_tensor:
-            self._x = np.array(x).astype(dtype)
+            self._x = np.resize(np.array(x).astype(dtype), (self._n_x, 1))
             self._n_x = len(self._x)
-            self._y = np.array(y).astype(dtype) if y is not None else np.array([])
+            self._y = np.resize(np.array(y).astype(dtype), (self._n_y, 1)) if y is not None else np.array([])
             self._n_y = len(self._y)
             self._dim = 1 + (self._n_y > 1)
         else:
@@ -96,10 +95,10 @@ class Grid:
 
             if y is not None:
                 if isinstance(y, torch.Tensor):
-                    self._y = torch.reshape(torch.tensor(x.detach().clone().numpy(), dtype=dtype),
+                    self._y = torch.reshape(torch.tensor(y.detach().clone().numpy(), dtype=dtype),
                                             (self._n_y, 1))
                 else:
-                    self._y = torch.reshape(torch.tensor(x, dtype=dtype), (self._n_y, 1))
+                    self._y = torch.reshape(torch.tensor(y, dtype=dtype), (self._n_y, 1))
 
         # Create datasets
         if y is None:
@@ -199,6 +198,12 @@ class Grid:
     def y(self):
         return self._y
 
+    @property
+    def volume(self):
+        if self.dim == 1:
+            return self._x[-1]-self._x[0]
+        elif self.dim == 2:
+            return (self._x[-1]-self._x[0])*(self._y[-1]-self._y[0])
 
 # ... Grid constructor ................................................................................................
 

Code_torch/Utils/VPINN.py
@@ -127,12 +127,12 @@ class VPINN(nn.Module):
 
         loss_v = torch.tensor(0.0, requires_grad=True)
 
-        laplace = torch.sum(self.gradgrad(grid.data, requires_grad=True), dim=1, keepdim=True)
+        laplace = torch.sum(self.gradgrad(grid.interior, requires_grad=True), dim=1, keepdim=True)
 
-        for i in range(len(f_integrated.data)):
-            q = integrate(laplace, test_func_vals.data[i]) + f_integrated.data[i]
-            q = torch.square(q.clone()) / len(f_integrated.data)
+        for i in range(f_integrated.size):
+            q = integrate(laplace, test_func_vals[i], grid.volume) - f_integrated.data[i]
+            q = torch.square(q.clone())
             loss_v = loss_v + q
             del q
 
-        return loss_v
+        return loss_v / len(f_integrated.data)

Code_torch/Utils/functions.py
@@ -2,12 +2,15 @@ from scipy.special import gamma
 from scipy.special import jacobi
 import numpy as np
 import torch
-from typing import Any, Union, Sequence
+from typing import Any, Sequence, Union
 
 # Import function decorator
+from .Types.DataSet import DataSet
+from .Types.Grid import Grid
 from .utils import adapt_input
 
 """Functions used in the VPINNS model"""
+
 
 # ... Exact solution ...................................................................................................
 
@@ -26,16 +29,18 @@ def u(x: Any) -> float:
     # Define the 1D case
     if len(x) == 1:
 
-        return np.sin(np.pi*x)
-        # r1, omega, amp = 5, 4 * np.pi, 1
-        # return amp * (0.1 * np.sin(omega * x) + np.tanh(r1 * x))
+        # return -np.sin(np.pi*x)
+        r1, omega, amp = 5, 4 * np.pi, 1
+        return amp * (0.1 * np.sin(omega * x) + np.tanh(r1 * x))
 
     # Define the 2D case
-    elif len(x) == 2 :
-        return np.sin(np.pi*x[0])*np.sin(np.pi*x[1])
+    elif len(x) == 2:
+        return x[0]*x[1]
+        #return np.cos(np.pi * x[0]) * np.cos(np.pi * x[1])
 
     else:
         raise ValueError(f"You have not configured the function 'u' to handle {len(x)}-dimensional inputs!")
+
 
 # ... External forcing .................................................................................................
 
@@ -53,13 +58,14 @@ def f(x: Any) -> float:
 
     # 1D example
     if len(x) == 1:
-        return np.pi**2*np.sin(np.pi*x)
-        # r1, omega, amp = 5, 4 * np.pi, 1
-        # return amp * (0.1 * (omega ** 2) * np.sin(omega * x)
-        #               + (2 * r1 ** 2) * np.tanh(r1 * x) / np.cosh(r1 * x) ** 2)
+        # return np.pi**2*np.sin(np.pi*x)
+        r1, omega, amp = 5, 4 * np.pi, 1
+        return -amp * (0.1 * (omega ** 2) * np.sin(omega * x)
+                      + (2 * r1 ** 2) * np.tanh(r1 * x) / np.cosh(r1 * x) ** 2)
     # 2D example
     elif len(x) == 2:
-        return 2*np.pi**2*np.sin(np.pi*x[0])*np.sin(np.pi*x[1])
+        return 0
+        #return -2 * np.pi ** 2 * np.cos(np.pi * x[0]) * np.cos(np.pi * x[1])
 
     else:
         raise ValueError(f"You have not configured the function 'f' to handle {len(x)}-dimensional inputs!")
@@ -79,11 +85,11 @@ def djacobi_poly(x: Any, n: int, *, d: int, a: int, b: int) -> Union[float, Sequ
     elif d >= n:
         return 0
     else:
-        return gamma(a+b+n+1+d)/(2**d*gamma(a+b+n+1))*jacobi_poly(x, n-d, a=a+d, b=b+d)
+        return gamma(a + b + n + 1 + d) / (2 ** d * gamma(a + b + n + 1)) * jacobi_poly(x, n - d, a=a + d, b=b + d)
 
 
 @adapt_input
-def test_function(point: Sequence, n) -> float:
+def test_function(point: Sequence, n: Union[int, Sequence[int]]) -> float:
     """Returns the test function evaluated at a grid point of arbitrary
     dimension. Higher dimensional test functions are simply products of
     1d test functions.
@@ -130,23 +136,77 @@ def dtest_function(point: Union[float, Sequence[float]], n, *, d: int) -> Union[
     return res
 
 
-# ... Function integration .............................................................................................
+# ... Function utilities ...............................................................................................
 
-def integrate(function_vals: Any, test_func_vals: Any, as_tensor: bool = True,
+
+def evaluate_test_funcs(grid: Grid, test_func_dim: Union[int, Sequence[int]]):
+    """ Evaluate the test functions on a grid. For higher dimensional grid functions, it is
+    sufficient to evaluate the test functions on the grid axes and then combine the test
+    function values together.
+
+    :param grid: the grid on which to evaluate the test functions
+    :param test_func_dim: the number of test functions in each direction
+    :return: the test function values and test function indices
+    """
+    n_test_funcs: int = test_func_dim if grid.dim == 1 else test_func_dim[0] * test_func_dim[1]
+
+    if grid.dim == 1:
+
+        # Get the test function indices
+        idx = [[i] for i in range(n_test_funcs)]
+
+        # Evaluate the test functions on the grid interior, as they vanish on the boundary
+        test_func_vals: Sequence = DataSet(coords=idx,
+                                           data=[test_function(grid.interior, i) for i in
+                                                 range(1, test_func_dim + 1)],
+                                           as_tensor=True, requires_grad=False).data
+
+    else:
+
+        # Get the test function indices in x and y direction
+        test_func_vals_x: Sequence = DataSet(coords=[[i] for i in range(test_func_dim[0])],
+                                             data=[test_function(grid.x[1:-1], i) for i in
+                                                   range(1, test_func_dim[0] + 1)],
+                                             as_tensor=True, requires_grad=False).data
+        test_func_vals_y: Sequence = DataSet(coords=[[i] for i in range(test_func_dim[1])],
+                                             data=[test_function(grid.y[1:-1], i) for i in
+                                                   range(1, test_func_dim[1] + 1)],
+                                             as_tensor=True, requires_grad=False).data
+
+        # Combine the indices
+        idx: Sequence = Grid(x=[i for i in range(test_func_dim[0])], y=[i for i in range(test_func_dim[1])],
+                             dtype=int, as_tensor=False).data.tolist()
+
+        # Generate the grid of test function values
+        test_func_vals: torch.Tensor = torch.from_numpy(np.array(
+            [Grid(x=test_func_vals_x[idx[i][0]].numpy(),
+                  y=test_func_vals_y[idx[i][1]].numpy(),
+                  dtype=np.float32, as_tensor=False).data
+             for i in range(n_test_funcs)]))
+
+        # Multiply test function values together on coordinates
+        test_func_vals = torch.reshape(torch.prod(test_func_vals, dim=2), (n_test_funcs, len(grid.interior), 1))
+        test_func_vals.requires_grad = False
+
+    return test_func_vals, idx
+
+
+def integrate(function_vals: Any, test_func_vals: Any, grid_vol: float, as_tensor: bool = True,
               dtype=torch.float, requires_grad: bool = False):
     """
     Integrates a function against a test function over a domain, using simple quadrature.
     :param function_vals: the function values on the domain.
     :param test_func_vals: the function values on the domain.
+    :param grid_vol: the volume of the grid
     :param as_tensor: whether to return the values as a torch.Tensor
     :param dtype: the data type to use.
     :param requires_grad: whether the return values requires differentiation.
     :return: the value of the integral
     """
     if not as_tensor:
-        return 1.0 / len(test_func_vals) * np.sum(function_vals * test_func_vals)
+        return grid_vol / len(test_func_vals) * np.sum(function_vals * test_func_vals)
     else:
-        res = 1.0 / len(test_func_vals) * torch.sum(function_vals * test_func_vals)
+        res = grid_vol / len(test_func_vals) * torch.sum(function_vals * test_func_vals)
         if isinstance(res, torch.Tensor):
             return torch.reshape(res, (1,))
         else:

Code_torch/Utils/plots.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import torch
 
@@ -35,15 +36,33 @@ def plot_prediction(grid: Grid, y_pred, grid_shape: tuple):
     # 2d heatmap
     elif grid.dim == 2:
 
-        fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(torch.reshape(u_exact(grid.data), grid_shape))
-        axs[1].imshow(torch.reshape(torch.flatten(y_pred), grid_shape))
+        fig, axs = plt.subplots(1, 3)
+        exact_solution = torch.reshape(u_exact(grid.data), grid_shape)
+        predicted_solution = torch.reshape(torch.flatten(y_pred), grid_shape)
+        err = np.abs(predicted_solution-exact_solution)
+        im1 = axs[0].imshow(exact_solution)
+        im2 = axs[1].imshow(predicted_solution)
+        im3 = axs[2].imshow(err)
+
+        divider = make_axes_locatable(axs[0])
+        cax = divider.append_axes('right', size='5%', pad=0.1)
+        fig.colorbar(im1, cax=cax, orientation='vertical')
+
+        divider = make_axes_locatable(axs[1])
+        cax = divider.append_axes('right', size='5%', pad=0.1)
+        fig.colorbar(im2, cax=cax, orientation='vertical')
+
+        divider = make_axes_locatable(axs[2])
+        cax = divider.append_axes('right', size='5%', pad=0.1)
+        fig.colorbar(im3, cax=cax, orientation='vertical')
 
         fig.suptitle('Exact and predicted solution')
 
         axs[0].set_title('Exact solution')
         axs[1].set_title('VPINN')
+        axs[2].set_title('Error')
         for ax in axs:
+            ax.yaxis.labelpad = 20
             ax.set_xlabel(r'$x$')
             ax.set_ylabel(r'$y$', rotation=0)
 
@@ -70,6 +89,8 @@ def plot_loss(loss_tracker: dict):
 
 # Plots the test functions
 def plot_test_functions(x: Grid, n_test_func: int, *, d: int = 0):
-    for i in range(1, n_test_func):
-        plt.plot(x.data, dtest_function(x.data, i, d=d))
-    plt.show()
+    if x.dim == 1:
+        for i in range(1, n_test_func):
+            plt.plot(x.data, dtest_function(x.data, i, d=d))
+        plt.show()
+

Code_torch/Utils/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 from typing import Any, Sequence
+
+# Local imports
 from .Types.Grid import Grid
 
 """Utility functions used in the VPINNS model"""
@@ -52,7 +54,7 @@ def adapt_input(func=None, *, dtype=torch.float, requires_grad: bool = False,
             # If x is a list of points
             else:
                 x = np.resize([x.detach().clone().numpy()], (len(x), len(x[0])))
-                res = [torch.tensor(func(val, *args, *kwargs), dtype=dtype, requires_grad=requires_grad) for val in x]
+                res = [torch.tensor(func(val, *args, **kwargs), dtype=dtype, requires_grad=requires_grad) for val in x]
                 return torch.reshape(torch.stack(res), (len(x), output_dim))
 
         # Evaluate on sequences that are not torch.Tensors
@@ -77,3 +79,4 @@ def adapt_input(func=None, *, dtype=torch.float, requires_grad: bool = False,
             return func(x, *args, **kwargs)
 
     return evaluate
+