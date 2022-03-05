Improve and correct calculation of test function derivatives

Code_torch/Utils/VPINN.py
@@ -144,7 +144,8 @@ class VPINN(nn.Module):
 
                 grad = self.grad(grid.interior, requires_grad=True)
                 for i in range(f_integrated.size):
-                    q = -grid.volume / len(grad) * torch.einsum('ij, ij->', grad, d1test_func_vals[i]) - f_integrated.data[i]
+                    q = (-1.0*grid.volume / len(d1test_func_vals[i]) * torch.einsum('ij, ij->', grad, d1test_func_vals[i])
+                         - f_integrated.data[i])
                     q = torch.square(q.clone())
                     loss_v = loss_v + q
                     del q

Code_torch/Utils/functions.py
@@ -1,15 +1,15 @@
+import numpy as np
 from scipy.special import gamma
 from scipy.special import jacobi
-import numpy as np
 import torch
 from typing import Any, Sequence, Union
 
-# Import function decorator
+# Local imports
 from .Types.DataSet import DataSet
 from .Types.Grid import Grid
 from .utils import adapt_input
 
-"""Functions used in the VPINNS model"""
+"""Functions used in the VPINN model"""
 
 
 # ... Exact solution ...................................................................................................
@@ -33,7 +33,8 @@ def u(x: Any) -> float:
 
     # Define the 2D case
     elif len(x) == 2:
-        return (0.1 * np.sin(2 * np.pi * x[0]) + np.tanh(10 * x[0])) * np.sin(2 * np.pi * x[1])
+        return np.sin(np.pi * x[0]) * np.sin(np.pi * x[1])
+        # return (0.1 * np.sin(2 * np.pi * x[0]) + np.tanh(10 * x[0])) * np.sin(2 * np.pi * x[1])
 
     else:
         raise ValueError(f"You have not configured the function 'u' to handle {len(x)}-dimensional inputs!")
@@ -60,11 +61,12 @@ def f(x: Any) -> float:
                        + (2 * r1 ** 2) * np.tanh(r1 * x) / np.cosh(r1 * x) ** 2)
     # 2D example
     elif len(x) == 2:
-        A, B = 0.1, 10
-        return (np.sin(2 * np.pi * x[1]) * (
-                -4 * np.pi ** 2 * A * np.sin(2 * np.pi * x[0]) - 2 * B ** 2 * np.tanh(B * x[0]) * np.cosh(
-            B * x[0]) ** (-2))
-                - 4 * np.pi ** 2 * np.sin(2 * np.pi * x[1]) * (A * np.sin(2 * np.pi * x[0]) + np.tanh(B * x[0])))
+        return -2 * np.pi ** 2 * u(x)
+        # A, B = 0.1, 10
+        # return (np.sin(2 * np.pi * x[1]) * (
+        #         -4 * np.pi ** 2 * A * np.sin(2 * np.pi * x[0]) - 2 * B ** 2 * np.tanh(B * x[0]) * np.cosh(
+        #     B * x[0]) ** (-2))
+        #         - 4 * np.pi ** 2 * np.sin(2 * np.pi * x[1]) * (A * np.sin(2 * np.pi * x[0]) + np.tanh(B * x[0])))
 
     else:
         raise ValueError(f"You have not configured the function 'f' to handle {len(x)}-dimensional inputs!")
@@ -88,7 +90,7 @@ def djacobi_poly(x: Any, n: int, *, d: int, a: int, b: int) -> Union[float, Sequ
 
 
 @adapt_input
-def test_function(point: Sequence, n: int) -> float:
+def test_function(point: Any, n: int) -> float:
     """Returns the test function evaluated at a grid point of arbitrary
     dimension. Higher dimensional test functions are simply products of
     1d test functions.
@@ -110,12 +112,15 @@ def dtest_function(point: Union[float, Sequence[float]], n: int, *, d: int, as_t
     """Returns a vector of partial of the nth partial derivatives of the test functions. Since the
     test functions are products of 1d test functions, the partial derivatives simplify considerably.
     Args:
-        point :Union[Sequence, float]: the coordinate values
-        n :int: the number of the test function
-        d :int: the order of the derivatives
+        :param point: the coordinate values
+        :param n: the index of the test function
+        :param d: the order of the derivative
+        :param as_tensor: whether to return a torch.Tensor
+        :param requires_grad: whether the return value requires differentiation
     Returns:
         a list of partial derivatives along each coordinate direction at each grid point. If there is only one
             coordinate dimension, the derivative of the function at that point is returned.
+
     """
 
     if d == 0:
@@ -126,6 +131,8 @@ def dtest_function(point: Union[float, Sequence[float]], n: int, *, d: int, as_t
         return djacobi_poly(x, n + 1, d=d, a=0, b=0) - djacobi_poly(x, n - 1, d=d, a=0, b=0)
 
     res = []
+
+    # To do: implement non-Tensor return type
     if as_tensor:
         # scalar
         if point.size() <= torch.Size([1]):
@@ -134,22 +141,21 @@ def dtest_function(point: Union[float, Sequence[float]], n: int, *, d: int, as_t
         # single point with multiple coords
         elif point.dim() == 1:
             for i in range(len(point)):
-                df_i = _dtest_function_1d(point[i], n, d=d)
+                df_i = 1.0
                 for j in range(len(point)):
                     if j == i:
-                        continue
-                    else:
                         df_i *= _dtest_function_1d(point[j], n, d=d)
+                    else:
+                        df_i *= test_function([point[j]], n)
                 res.append(df_i)
             return torch.tensor(res, requires_grad=requires_grad, dtype=dtype)
 
         # list of points
         else:
-            q = point.size()
             for p in point:
                 res.append(dtest_function(p, n, d=d))
-            v = torch.reshape(torch.tensor(res, requires_grad=requires_grad, dtype=dtype), (len(point), point.size()[-1]))
-            return v
+            return torch.reshape(torch.stack(res), (len(point), point.size()[-1]))
+
 
 # ... Function utilities ...............................................................................................
 
@@ -162,6 +168,7 @@ def evaluate_test_funcs(grid: Grid, test_func_dim: Union[int, Sequence[int]], *,
     :param grid: the grid on which to evaluate the test functions
     :param test_func_dim: the number of test functions in each direction
     :param d: the derivative of the test function to use
+    :param output_dim: the output dimension of the function
     :return: the test function values and test function indices
     """
     n_test_funcs: int = test_func_dim if grid.dim == 1 else test_func_dim[0] * test_func_dim[1]
@@ -180,10 +187,19 @@ def evaluate_test_funcs(grid: Grid, test_func_dim: Union[int, Sequence[int]], *,
     else:
 
         # Get the test function indices in x and y direction
+        test_func_vals_x_0: Sequence = DataSet(coords=[[i] for i in range(test_func_dim[0])],
+                                               data=[dtest_function(grid.x[1:-1], i, d=0) for i in
+                                                     range(1, test_func_dim[0] + 1)],
+                                               as_tensor=True, requires_grad=False).data
         test_func_vals_x: Sequence = DataSet(coords=[[i] for i in range(test_func_dim[0])],
                                              data=[dtest_function(grid.x[1:-1], i, d=d) for i in
                                                    range(1, test_func_dim[0] + 1)],
                                              as_tensor=True, requires_grad=False).data
+
+        test_func_vals_y_0: Sequence = DataSet(coords=[[i] for i in range(test_func_dim[1])],
+                                               data=[dtest_function(grid.y[1:-1], i, d=0) for i in
+                                                     range(1, test_func_dim[1] + 1)],
+                                               as_tensor=True, requires_grad=False).data
         test_func_vals_y: Sequence = DataSet(coords=[[i] for i in range(test_func_dim[1])],
                                              data=[dtest_function(grid.y[1:-1], i, d=d) for i in
                                                    range(1, test_func_dim[1] + 1)],
@@ -194,17 +210,26 @@ def evaluate_test_funcs(grid: Grid, test_func_dim: Union[int, Sequence[int]], *,
                              dtype=int, as_tensor=False).data.tolist()
 
         # Generate the grid of test function values
-        test_func_vals: torch.Tensor = torch.from_numpy(np.array(
-            [Grid(x=test_func_vals_x[idx[i][0]].numpy(),
-                  y=test_func_vals_y[idx[i][1]].numpy(),
-                  dtype=np.float32, as_tensor=False).data
-             for i in range(n_test_funcs)]))
+        if d == 0:
+            test_func_vals: torch.Tensor = torch.from_numpy(np.array(
+                [Grid(x=test_func_vals_x[idx[i][0]].numpy(),
+                      y=test_func_vals_y[idx[i][1]].numpy(),
+                      dtype=np.float32, as_tensor=False).data
+                 for i in range(n_test_funcs)]))
+            test_func_vals = torch.reshape(torch.prod(test_func_vals, dim=2),
+                                           (n_test_funcs, len(grid.interior), 1))
+        else:
 
-        # Multiply test function values together on coordinates
-        if output_dim == 1:
-            test_func_vals = torch.reshape(torch.prod(test_func_vals, dim=2), (n_test_funcs, len(grid.interior), output_dim))
-        # else:
-        #     test_func_vals = torch.reshape(test_func_vals, (n_test_funcs, len(grid.interior), output_dim))
+            test_func_vals = []
+            for k in idx:
+                for y in range(len(grid.y[1:-1])):
+                    for x in range(len(grid.x[1:-1])):
+                        v = [test_func_vals_x[k[0]][x] * test_func_vals_y_0[k[1]][y],
+                             test_func_vals_x_0[k[0]][x] * test_func_vals_y[k[1]][y]]
+                        test_func_vals.append(torch.reshape(torch.stack(v), (2, )))
+
+            test_func_vals = torch.reshape(torch.stack(test_func_vals), (n_test_funcs, len(grid.interior), 2))
+
         test_func_vals.requires_grad = False
 
     return test_func_vals, idx

Code_torch/Utils/plots.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
+import seaborn as sns
 import torch
 
 from .functions import test_function, dtest_function, f, u as u_exact
@@ -31,43 +32,43 @@ def plot_prediction(grid: Grid, y_pred, grid_shape: tuple):
         # Draw x-axis and grid
         plt.axhline(0, linewidth=1, linestyle='-', color='black')
         plt.grid()
+        plt.savefig('Results/results_1d.pdf')
+        plt.close()
 
 
     # 2d heatmap
     elif grid.dim == 2:
 
-        fig, axs = plt.subplots(1, 3)
+        plot_titles = ['Exact solution', 'VPINN prediction', 'Pointwise error', 'Forcing']
+        fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+        axs = np.resize(axs, (1, 4))[0]
+
         exact_solution = torch.reshape(u_exact(grid.data), grid_shape)
         predicted_solution = torch.reshape(torch.flatten(y_pred), grid_shape)
         err = np.abs(predicted_solution-exact_solution)
         forcing = torch.reshape(f(grid.data), grid_shape)
-        im1 = axs[0].imshow(exact_solution)
-        im2 = axs[1].imshow(predicted_solution)
-        im3 = axs[2].imshow(err)
 
-        divider = make_axes_locatable(axs[0])
-        cax = divider.append_axes('right', size='5%', pad=0.1)
-        fig.colorbar(im1, cax=cax, orientation='vertical')
+        extent = (grid.x[0].numpy()[0], grid.x[-1].numpy()[0], grid.y[0].numpy()[0], grid.y[-1].numpy()[0])
+        im1 = axs[0].imshow(exact_solution, origin='lower', extent=extent, cmap=sns.color_palette("rocket", as_cmap=True))
+        im2 = axs[1].imshow(predicted_solution, origin='lower', extent=extent, cmap=sns.color_palette("rocket", as_cmap=True))
+        im3 = axs[2].imshow(err, origin='lower', extent=extent, cmap=sns.color_palette("rocket", as_cmap=True))
+        im4 = axs[3].imshow(forcing, origin='lower', extent=extent, cmap=sns.color_palette("rocket", as_cmap=True))
 
-        divider = make_axes_locatable(axs[1])
-        cax = divider.append_axes('right', size='5%', pad=0.1)
-        fig.colorbar(im2, cax=cax, orientation='vertical')
+        imgs = [im1, im2, im3, im4]
 
-        divider = make_axes_locatable(axs[2])
-        cax = divider.append_axes('right', size='5%', pad=0.1)
-        fig.colorbar(im3, cax=cax, orientation='vertical')
+        for i in range(len(imgs)):
+            divider = make_axes_locatable(axs[i])
+            cax = divider.append_axes('right', size='5%', pad=0.1)
+            fig.colorbar(imgs[i], cax=cax, orientation='vertical')
+            axs[i].set_title(plot_titles[i])
+            if i > 2:
+                axs[i].set_xlabel(r'$x$')
+            if i == 0 or i == 3:
+                axs[i].set_ylabel(r'$y$', rotation=0)
+                axs[i].yaxis.labelpad = 10
 
-        fig.suptitle('Exact and predicted solution')
-
-        axs[0].set_title('Exact solution')
-        axs[1].set_title('VPINN')
-        axs[2].set_title('Pointwise error')
-        for ax in axs:
-            ax.yaxis.labelpad = 20
-            ax.set_xlabel(r'$x$')
-            ax.set_ylabel(r'$y$', rotation=0)
-
-    plt.show()
+        fig.savefig('Results/results_2d.pdf')
+        plt.close()
 
 
 # Plots the loss over time
@@ -88,18 +89,43 @@ def plot_loss(loss_tracker: dict):
     plt.show()
 
 
-# Plots the test functions
-def plot_test_functions(x: Grid, grid: Grid, n_test_func: int, test_func_vals, *, d: int = 0):
-    if x.dim == 1:
-        for i in range(1, n_test_func+1):
-            plt.plot(x.data, test_function(x.data, i))
-            plt.plot(grid.interior.detach(), test_func_vals[i-1], marker='x', markersize=10, linewidth=0)
-            plt.show()
-    elif x.dim == 2:
-        for i in range(n_test_func):
-            plt.imshow(torch.reshape(test_func_vals[i], (198, 198)))
-            plt.show()
-        # for i in range(1, n_test_func):
-        #     plt.plot(x.data, dtest_function(x.data, i, d=d))
-        # plt.show()
+# Plots the test functions and their derivatives up to order n
+def plot_test_functions(grid: Grid, *, order: int, d: int = 1):
+
+    fig, axs = plt.subplots(int(order / 2), 2, sharex=True)
+    axs = np.resize(axs, (1, order))[0]
+
+    if grid.dim == 1:
+
+        fig.suptitle(fr'First {order} test functions and derivatives')
+
+        for i in range(order):
+            axs[i].axhline(0, linewidth=1, linestyle='-', color='black')
+            axs[i].plot(grid.data, test_function(grid.data, i+1), color='peru', label=fr'$v_{str(i)}(x)$')
+            if d > 0:
+                label_derivs = fr'$v^\prime_{str(i)}(x)$' if d==1 else fr'$d^{str(d)}v_{str(i)}(x)$'
+                axs[i].plot(grid.data, dtest_function(grid.data, i+1, d=d), color='darkred', label=label_derivs)
+            axs[i].legend(ncol=2)
+
+        plt.show()
+        fig.savefig('Results/test_functions_1d.pdf')
+
+    elif grid.dim == 2:
+
+        if d > 0:
+            fig.suptitle(fr'Derivatives of order {d} of test functions')
+        else:
+            fig.suptitle(fr'First {order} test functions')
+
+        extent = (grid.x[0].numpy()[0], grid.x[-1].numpy()[0], grid.y[0].numpy()[0], grid.y[-1].numpy()[0])
+        for i in range(order):
+            img = axs[i].imshow(torch.reshape(torch.prod(dtest_function(grid.data, i+1, d=d), dim=1), (len(grid.x), len(grid.y))),
+                       cmap=sns.color_palette("mako", as_cmap=True), origin='lower', extent=extent)
+            divider = make_axes_locatable(axs[i])
+            cax = divider.append_axes('right', size='5%', pad=0.1)
+            fig.colorbar(img, cax=cax, orientation='vertical')
+
+        plt.show()
+        fig.savefig('Results/dtest_functions_2d.pdf')
+
 