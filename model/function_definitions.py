0b6f4de173e3e62ce24085c8d2a534e62373ccbe
Thomas Gaskin <tgaskin@live.co.uk>
2022-10-11 22:18:17 +0100

    Changes to Poisson defaults

--- model/function_definitions.py
+++ model/function_definitions.py
@@ -44,7 +44,5 @@ EXAMPLES = {
 # Dictionary of weight functions
 WEIGHT_FUNCTIONS = {
     "uniform": lambda x: torch.tensor(1.0, dtype=torch.float64),
-    "exponential": lambda x: torch.tensor(
-        2.0 ** (-x[0]) * 2.0 ** (-x[1]), dtype=torch.float64
-    ),
+    "exponential": lambda x: torch.tensor(2.0 ** (-sum(x)), dtype=torch.float64),
 }
